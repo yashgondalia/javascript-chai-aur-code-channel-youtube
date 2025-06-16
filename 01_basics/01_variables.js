261cb18a60d878bb142e0a4413b7d0d3a440be77
unknown <yashgondalia1981@gmail.com>
2025-06-16 11:14:52 +0530

    all files

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,14 +1,12 @@
-const accountId = 144553
-let accountEmail = "yash@google.account"
-var accountPassword = "12345"
-accountCity = "jaipur"
+const accountId = 144553;
+let accountEmail = "yash@google.account";
+var accountPassword = "12345";
+accountCity = "jaipur";
 
-accountEmail = "yash@mail.com"
-accountPassword = "2344"
-accountCity = "mumbai"
-
+accountEmail = "yash@mail.com";
+accountPassword = "2344";
+accountCity = "mumbai";
 
 console.log(accountId);
 
-console.log([accountId, accountEmail, accountPassword
-
+console.log([accountId, accountEmail, accountPassword]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3); // code readability should be high
+
+console.log("Hitesh")
+
+let name = "yash"
+let age = 20  
+let isLoggedIn = false
+let state = null
+
+console.log(name,age,isLoggedIn,state);
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// null => standalone value
+// undefined
+// symbol => unique
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);//object
+
+
+
+
+
