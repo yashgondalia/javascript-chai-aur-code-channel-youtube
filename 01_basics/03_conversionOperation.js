8c2d9145732659baef24b50c88311739ab4f43c6
unknown <yashgondalia1981@gmail.com>
2025-06-16 11:43:00 +0530

    all files

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = false
+//console.log(score);
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(valueInNumber);
+
+
+//"33 => 33"
+//"33abc" => NaN
+//true => 1; false = 0
+
+
+let isLoggedIn = "yash"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn)
