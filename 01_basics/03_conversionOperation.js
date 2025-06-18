eb39979f9982bffa2e17ed2356b38a5cdb04b68a
unknown <yashgondalia1981@gmail.com>
2025-06-18 19:56:57 +0530

    all changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -11,8 +11,33 @@ let valueInNumber = Number(score)
 //true => 1; false = 0
 
 
-let isLoggedIn = "yash"
+let isLoggedIn = true
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
-console.log(typeof booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn);
+//console.log(typeof booleanIsLoggedIn)
+
+
+//******************Operations**************** */
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+//console.log(2+2);
+//console.log(2-2)
+
+let str1 = "hello"
+let str2 = " yash"
+let str3 = str1 + str2 
+//console.log(str3);
+
+// console.log("1"+"2");
+// console.log(1+"2");
+// console.log((1+2)+3+"1");
+// console.log("1"+2+2+2);
+
+let gameCounter =100
+gameCounter++
+console.log(gameCounter);
+
+
