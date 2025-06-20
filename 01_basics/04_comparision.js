28c523edd372835af0f344c89bcc94981f223247
unknown <yashgondalia1981@gmail.com>
2025-06-20 10:26:57 +0530

    datatype summary

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -17,8 +17,5 @@
 // console.log(undefined>0);
 // console.log(undefined<0);
 
-console.log("2"===2);
-
-
-
+console.log("2"===2)
 
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+// primitive
+
+//7 types: String,Number,Boolean,Null,Undefined,Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail = undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//  console.log(id === anotherId);
+
+//const bigNumber = 234567867546767567456574n
+
+//Refrence(Non Proimitive)
+
+//Array,Objects,Functions
+const heros = ["shaktiman","nagraj","doga"]
+let myObj = {
+  name: "yash",
+  age: 22,
+}
+
+const myFunction = function() {
+  console.log("hello world");
+  
+}
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof anotherId);
+
+
